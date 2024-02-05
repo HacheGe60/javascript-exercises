70a7cf008613def481b777a42429ea4c792a338f
lolodev <horaciodev1706@gmail.com>
2024-02-05 18:01:48 -0300

    Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num_1, num_2) {
+    let sum = 0;
+    if (typeof num_1 !== 'number' || typeof num_2 !== 'number' || num_1 < 0 || num_2 < 0) {
+        return "ERROR";
+    }
+    if (num_1 < num_2) {
+        for (let i = num_1; i <= num_2; i++) {
+            sum += i;
+        }
+    } else if (num_1 > num_2) {
+        for (let i = num_2; i <= num_1; i++) {
+            sum += i;
+        }
+    } else {
+        sum = num_1 + num_2;
+    }
+    return sum;
 
 };
 
